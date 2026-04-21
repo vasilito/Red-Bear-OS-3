7f2979f25e43230a4dee3bd5694e61a1dee501ae
Vasilito <adminpupkin@gmail.com>
2026-04-21 16:15:17 +0100

    Refresh KDE source compatibility patches
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- local/recipes/kde/kf6-kio/source/src/core/workerinterface.cpp
+++ local/recipes/kde/kf6-kio/source/src/core/workerinterface.cpp
@@ -12,6 +12,12 @@
 #include "kiocoredebug.h"
 
 #include <QHostInfo>
+
+#include <QHostInfo>
+
+#include <QHostInfo>
+
+#include <QHostInfo>
 #include "usernotificationhandler_p.h"
 #include "workerbase.h"
 
